8b7c2765d47c28b08a7cf270c337e418ecb05f7e
kangwhon <kangwhon@naver.com>
2024-04-16 15:39:42 +0900

    Create : project

---
+++ RealVoice/src/main/java/com/example/RealVoice/RealVoiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.RealVoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealVoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealVoiceApplication.class, args);
+	}
+
+}
