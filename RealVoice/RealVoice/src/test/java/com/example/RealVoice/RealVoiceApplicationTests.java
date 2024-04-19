01d8e402817622ea583ba5980f99334f38f289c3
kangwhon <kangwhon@naver.com>
2024-04-19 15:59:47 +0900

    update

---
+++ RealVoice/RealVoice/src/test/java/com/example/RealVoice/RealVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RealVoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealVoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
